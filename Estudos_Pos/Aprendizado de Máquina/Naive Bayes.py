aa4105d46076a7a3153b92d4a9ed369045ec9d00
aboutdouglas <37315607+aboutdouglas@users.noreply.github.com>
2022-06-20 13:49:07 -0300

    naive Bayes

---
+++ Estudos_Pos/Aprendizado de Máquina/Naive Bayes.py
@@ -0,0 +1,23 @@
+# Importando Bibliotecas
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# Importando o modelo Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+
+# Carregando os dados
+X, y = load_iris(return_X_y=True)
+
+# Separando a base de treino da validação
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5, random_state=0)
+
+# Criando classificadores
+gnb = GaussianNB()
+
+# Treinando o classificador
+gnb.fit(X_train, y_train)
+
+# Realizando as previsões
+y_pred = gnb.predict(X_test)
+
+print(y_pred)
