769909e4da75bdb65784e5201507197051c7fb93
aboutdouglas <37315607+aboutdouglas@users.noreply.github.com>
2022-06-20 15:54:12 -0300

    AD

---
+++ Estudos_Pos/Aprendizado de Máquina/Árvore de Decisão.py
@@ -0,0 +1,24 @@
+# importando as bibliotecas
+from json import load
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# importando o modelo árvore de decisão
+from sklearn.tree import DecisionTreeClassifier
+
+# carregando os dados
+X, y = load_iris(return_X_y=True)
+
+# separando a base de treinamento da validação
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+
+# criando o classificador
+dtc = DecisionTreeClassifier()
+
+# treinando o classificador
+dtc.fit(X_train, y_train)
+
+# realizando as previsões
+y_pred = dtc.predict(X_test)
+
+print(y_pred)
