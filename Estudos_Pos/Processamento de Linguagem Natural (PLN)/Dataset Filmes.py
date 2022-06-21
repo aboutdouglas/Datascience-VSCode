4b68315a0f6ad5872730fc15552b1094e8e84088
aboutdouglas <37315607+aboutdouglas@users.noreply.github.com>
2022-06-21 16:15:27 -0300

    Dataset Filmes

---
+++ Estudos_Pos/Processamento de Linguagem Natural (PLN)/Dataset Filmes.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+filmes = pd.read_csv('C:\\Users\QUALITOR.QPOA000006\Desktop\Python\Estudos_Pos\Processamento de Linguagem Natural (PLN)\IMDB_Dataset.csv', header=0)
+print(filmes.head())
+
+print(filmes.isnull().sum())
+
+print(filmes['sentiment'].value_counts())
+
+filmes['sentiment'].value_counts().plot.bar(title='Quantidade por Tipo',rot=90)
+plt.show()
